2a902623ee5e3bf444082f021ea83625a10a4c64
Tohew <baophong2401@gmail.com>
2023-04-13 10:27:43 +0700

    Update

---
+++ Bai009-Quang/Bai009.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+float Tong(float, int);
+
+int main()
+{
+	int k;
+	cout << "Nhap so nguyen: ";
+	cin >> k;
+	float y;
+	cout << " Nhap x: ";
+	cin >> y;
+	float kq = Tong(y, k);
+	cout << "Tong la: " << kq;
+	return 1;
+}
+float Tong(float x, int n)
+{
+	if (n == 0)
+		return 1;
+	return (Tong(x, n - 1) * x);
+}
---
+++ Bai021-Quang/Bai021.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+float Tong(float, int);
+
+int main()
+{
+	int k;
+	float y;
+	cout << "Nhap so nguyen: ";
+	cin >> k;
+	cin >> y;
+	float kq = Tong(y, k);
+	cout << "Ket qua la: " << kq;
+	return 1;
+}
+
+float Tong(float x, int n)
+{
+	if (n == 0)
+		return x;
+	return (Tong(x, n - 1) * (x + n));
+}
