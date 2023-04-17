f0941a039f355ede435b5c589734f53431b3f987
Terry <se.terry.2004@gmail.com>
2023-04-17 08:55:36 +0700

    Update by seterry

--- Bai058-Ngan/Bai058-Ngan.cpp
+++ Bai058-Ngan/Bai058-Ngan.cpp
@@ -9,7 +9,7 @@ float Tinh(float x, int n)
 	if (n == 0)
 		return 1;
 	if (n == 1)
-		return 1+x*x/2;
+		return 1 + x * x / 2;
 	float a = Tinh(x, n - 1);
 	float b = Tinh(x, n - 2);
 	float hs = x * x / (2 * n - 1) / (2 * n);
